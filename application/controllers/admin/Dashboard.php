87425b0040be19fdade0ddbb94f197b59ddaae29
MicoPratamaPutra <114336571+MicoPratamaPutra@users.noreply.github.com>
2022-09-25 17:01:58 +0700

    Update Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -6,6 +6,10 @@ class Dashboard extends CI_Controller
 public function index()
 {
     $this->load->view('templates/admin_header');
-$this->load->view('admin/dashboard'); 
+    $this->load->view('templates/topbar');
+    $this->load->view('templates/admin_sidebar');
+    $this->load->view('admin/dashboard'); 
+    $this->load->view('templates/admin_footer');
+}
 }
 }
